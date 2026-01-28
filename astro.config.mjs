fb67fd23aa0b8f7f2d95fdc5632c50dbea5df0b9
WU-YU-HUA <a0973350996@gmail.com>
2026-01-29 01:21:13 +0800

    adjust astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
-    adapter: vercel(),
-    output: 'hybrid'
+    adapter: vercel({
+        // 關鍵設定：強制讓每個路由生成獨立的 Function
+        functionPerRoute: true, 
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
+    output: 'server'
 });
