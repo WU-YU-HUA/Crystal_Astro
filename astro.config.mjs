10141a9f6e87930164ce4cc2af1bfb52525ed5d8
WU-YU-HUA <a0973350996@gmail.com>
2026-01-29 01:55:52 +0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-    adapter: vercel({
-        // 確保這兩項設定同時存在
-        functionPerRoute: true, 
-        // 強制使用獨立 Function 的策略
-        deploymentStrategy: 'static', 
-        webAnalytics: {
-            enabled: true,
-        },
-    }),
+    adapter: vercel(),
     output: 'server'
 });
