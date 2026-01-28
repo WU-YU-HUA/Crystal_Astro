03c5a1a2b3efa5381bc94666721edf196b68142f
WU-YU-HUA <a0973350996@gmail.com>
2026-01-29 02:29:28 +0800

    Setting server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel'; // 確保已安裝此套件
 
 export default defineConfig({
-    output: 'hybrid', // 必須開啟 server 模式才能執行 API
+    output: 'server', // 必須開啟 server 模式才能執行 API
     adapter: vercel(),
 });
