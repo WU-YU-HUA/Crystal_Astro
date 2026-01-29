cd8178ef510c7e13dbaaab2f7b38a6e9b8846c8f
WU-YU-HUA <a0973350996@gmail.com>
2026-01-29 16:05:10 +0800

    test vercel/serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server', // 必須保留，否則 API 無法運作
-  // adapter: vercel({
-  //   functionPerRoute: true, // 核心：將 API 拆分成獨立 Function
-  //   edge: false, // 確保使用標準 Serverless Functions
-  // }),
+  adapter: vercel(),
 });
